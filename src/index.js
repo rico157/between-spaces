c1023dbe3da225fd41c11bf8b06281fe8f00af0f
Riccardo Cogoni <cogonir@live.com>
2020-11-29 20:18:17 +0000

    create window on tray click

--- src/index.js
+++ src/index.js
@@ -3,29 +3,42 @@ const path = require("path");
 const server = require("./server");
 
 const PORT = 8080;
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
-  app.quit();
-}
 
 // Get local IP address
 require("dns").lookup(require("os").hostname(), function (err, add, fam) {
   console.log(add);
 });
 
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  // mainWindow.webContents.openDevTools();
+  app.dock.show();
+};
 app.on("ready", () => {
-  const createWindow = new BrowserWindow({ width: 800, height: 600 });
   // Set client server
   server.listen(PORT, () => console.log("listening"));
 
   let tray = null;
-  tray = new Tray("/Users/rico/icon.png");
+  tray = new Tray(__dirname + "/icon.png");
 
   const contextMenu = Menu.buildFromTemplate([
     {
       label: "Get QR code",
-      click: () => createWindow.loadURL(`index.html`)
+      click: () => createWindow()
     }
   ]);
   tray.setToolTip("This is my application.");
